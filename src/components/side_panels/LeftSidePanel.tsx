0177aa55246228c9eeb99fcad20fbd73e425c14a
laythra <laythra@gmail.com>
2023-12-16 17:56:19 +0300

    Add preliminary LeftSidePanel component

---
+++ src/components/side_panels/LeftSidePanel.tsx
@@ -0,0 +1,17 @@
+import { UserOutlined } from '@ant-design/icons';
+import SidePanel from 'components/side_panels/SidePanel';
+import IAMSidePanelNode from 'components/nodes/IAMSidePanelNode';
+import { Grid } from '@chakra-ui/react';
+
+export default function LeftSidePanel() {
+  return (
+    <SidePanel>
+      <Grid templateColumns={{ base: "repeat(1, 1fr)", lg: "repeat(2, 1fr)" }} gap={6}>
+        <IAMSidePanelNode label='I dont want to be the one' icon={UserOutlined} iconName='UserOutlined' />
+        <IAMSidePanelNode label='The battles always choose' icon={UserOutlined} iconName='UserOutlined' />
+        <IAMSidePanelNode label='IAM breaking the habit' icon={UserOutlined} iconName='UserOutlined' />
+        <IAMSidePanelNode label='tonight' icon={UserOutlined} iconName='UserOutlined' />
+      </Grid>
+    </SidePanel>
+  );
+}
