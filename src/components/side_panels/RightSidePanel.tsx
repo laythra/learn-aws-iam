c8905e8e2b5b14da995d5cbebaa9f157df49361c
laythra <laythra@gmail.com>
2023-12-25 21:45:55 +0300

    Add preliminary RightSidePanel component

---
+++ src/components/side_panels/RightSidePanel.tsx
@@ -0,0 +1,16 @@
+import { useContext } from 'react';
+import { Text } from '@chakra-ui/react';
+import { NodeContext } from 'components/nodes/NodeProvider';
+import SidePanel from 'components/side_panels/SidePanel';
+
+export default function LeftSidePanel() {
+  const { selectedNode } = useContext(NodeContext);
+
+  return (
+    <SidePanel alignment='right'>
+      <Text>
+        { selectedNode.description }
+      </Text>
+    </SidePanel>
+  );
+}
