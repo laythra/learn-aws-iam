24d40dc78ddcd71b2f0176b4c92e80eb50213510
laythra <laythra@gmail.com>
2023-12-29 22:28:54 +0300

    Move side panels toggle buttons into canvas

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,14 +4,21 @@ import { Flex } from '@chakra-ui/react';
 import 'reactflow/dist/style.css';
 import Canvas from 'components/Canvas';
 import LeftSidePanel from 'components/side_panels/LeftSidePanel';
+import LeftSidePanelToggleButton from 'components/side_panels/LeftSidePanelToggleButton';
 import RightSidePanel from 'components/side_panels/RightSidePanel';
+import RightSidePanelToggleButton from 'components/side_panels/RightSidePanelToggleButton';
+import SidePanelProvider from 'components/side_panels/SidePanelsProvider';
 
 const Home: React.FC = () => {
   return (
     <Flex h='100vh' color='blue.100'>
-      <LeftSidePanel />
-      <Canvas />
-      <RightSidePanel />
+      <SidePanelProvider>
+        <LeftSidePanel />
+        <LeftSidePanelToggleButton />
+        <Canvas />
+        <RightSidePanel />
+        <RightSidePanelToggleButton />
+      </SidePanelProvider>
     </Flex>
   );
 };
