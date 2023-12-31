2f9003f330a828e257b6c6af4b1607f9a358fd4f
laythra <laythra@gmail.com>
2023-12-31 14:57:49 +0300

    Render node label inside right side panel and add some preliminary styling

--- src/components/side_panels/RightSidePanel.tsx
+++ src/components/side_panels/RightSidePanel.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 
-import { Text } from '@chakra-ui/react';
+import { Text, Flex, Divider, Code, Box } from '@chakra-ui/react';
 import { IAMNodeContext } from 'components/nodes/IAMNodeProvider';
 import SidePanel from 'components/side_panels/SidePanel';
 
@@ -9,7 +9,15 @@ const LeftSidePanel: React.FC = () => {
 
   return (
     <SidePanel alignment='right'>
-      <Text>{selectedNode.description}</Text>
+      <Flex direction='column' alignItems='center'>
+        <Code fontSize='lg' fontWeight='bold' p={1}>
+          {selectedNode.label}
+        </Code>
+        <Divider my={2} />
+        <Box marginTop={2}>
+          <Text>{selectedNode.description}</Text>
+        </Box>
+      </Flex>
     </SidePanel>
   );
 };
