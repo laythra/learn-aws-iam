ad11a858684e4b40e0bbc4e60a122a3441adddaf
laythra <laythra@gmail.com>
2024-01-20 16:58:28 +0300

    Modify useCodeEditor hook to manage errors state

--- src/hooks/useCodeEditor.tsx
+++ src/hooks/useCodeEditor.tsx
@@ -1,19 +1,26 @@
+import { useState } from 'react';
+
+import { Diagnostic } from '@codemirror/lint';
 import useModal from 'hooks/useModal';
 import { ModalContextState } from 'types';
 
-type entityType = 'policy' | 'role';
 interface CodeEditorContextState extends ModalContextState {
-  entity: entityType;
+  policyErrors: Diagnostic[];
+  roleErrors: Diagnostic[];
+  setPolicyErrors: React.Dispatch<React.SetStateAction<Diagnostic[]>>;
+  setRoleErrors: React.Dispatch<React.SetStateAction<Diagnostic[]>>;
 }
 
-const useCodeEditor = (entity: entityType): CodeEditorContextState => {
+const useCodeEditor = (): CodeEditorContextState => {
   const context = useModal();
+  const [policyErrors, setPolicyErrors] = useState<Diagnostic[]>([]);
+  const [roleErrors, setRoleErrors] = useState<Diagnostic[]>([]);
 
   if (!context) {
     throw new Error('useCodeEditor must be used within a ModalProvider');
   }
 
-  return { ...context, entity };
+  return { ...context, policyErrors, roleErrors, setPolicyErrors, setRoleErrors };
 };
 
 export default useCodeEditor;
