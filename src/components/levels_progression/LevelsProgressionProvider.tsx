8123b05ae70101241f001442066e35b40009b763
Laith Al-Rachwani <laythra@gmail.com>
2024-03-11 23:31:18 +0300

    Add and use Levels progression provider which uses an xstate actor context (#9)

---
+++ src/components/levels_progression/LevelsProgressionProvider.tsx
@@ -0,0 +1,13 @@
+import { createActorContext } from '@xstate/react';
+import levelProgressionMachine from 'machines/levels-progression-machine';
+
+export const LevelsProgressionContext = createActorContext(levelProgressionMachine);
+interface LevelsProgressionProviderProps {
+  children: React.ReactNode;
+}
+
+const LevelsProgressionProvider: React.FC<LevelsProgressionProviderProps> = ({ children }) => {
+  return <LevelsProgressionContext.Provider>{children}</LevelsProgressionContext.Provider>;
+};
+
+export default LevelsProgressionProvider;
