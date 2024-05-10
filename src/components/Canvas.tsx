934cffeeefb68fa86e3a9a60fb0cbb68a035a7ef
laythra <laythra@gmail.com>
2024-05-10 19:51:07 +0300

    Change dummy data present in the canvas around a little bit

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react';
 
 import { Box } from '@chakra-ui/react';
+import { useTheme } from '@chakra-ui/react';
 import ReactFlow, { useNodesState, useEdgesState, addEdge, Edge, Connection } from 'reactflow';
 import styled from 'styled-components';
 
@@ -9,7 +10,7 @@ import DotsPattern from '@/assets/images/dots_pattern.svg';
 import IAMCanvasNode from '@/components/nodes/IAMCanvasNode';
 import { IAMNodeEntity } from '@/types';
 
-const CavnasWrapper = styled(Box)`
+const CanvasBackground = styled(Box)`
   position: relative;
   width: 100vw;
   height: 100vh;
@@ -24,22 +25,32 @@ const nodeTypes = {
 };
 
 const Canvas: React.FC = () => {
+  const theme = useTheme();
   const initialNodes = [
     {
-      id: '100',
-      position: { x: 0, y: 0 },
-      data: { id: '100', label: 'Test Node1', entity: IAMNodeEntity.User },
+      id: '1',
+      position: { x: 250, y: 250 },
+      data: { label: 'Test Node1', entity: IAMNodeEntity.User },
+      style: { zIndex: 10 },
       type: 'iam_default',
     },
     {
-      id: '200',
-      position: { x: 0, y: 100 },
-      data: { id: '200', label: 'Test Node2', entity: IAMNodeEntity.User },
+      id: '2',
+      position: { x: 500, y: 0 },
+      data: { label: 'Test Node2', entity: IAMNodeEntity.User },
+      style: { zIndex: 10 },
+      type: 'iam_default',
+    },
+    {
+      id: '3',
+      position: { x: 750, y: 0 },
+      data: { label: 'Test Node3', entity: IAMNodeEntity.User },
+      style: { zIndex: 10 },
       type: 'iam_default',
     },
   ];
 
-  const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+  const initialEdges = [] as Edge[];
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
@@ -73,17 +84,19 @@ const Canvas: React.FC = () => {
   );
 
   return (
-    <CavnasWrapper onDragOver={handleDragOver} onDrop={handleDrop}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        nodeTypes={nodeTypes}
-      />
+    <CanvasBackground onDragOver={handleDragOver} onDrop={handleDrop}>
+      <Box position='relative' height='100%' width='100%' zIndex={theme.zIndices.popover}>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          nodeTypes={nodeTypes}
+        />
+      </Box>
       <Logo />
-    </CavnasWrapper>
+    </CanvasBackground>
   );
 };
 
