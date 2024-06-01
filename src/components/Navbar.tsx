1ac37188d92ca1244fe3749f98ba0646993ca13c
laythra <laythra@gmail.com>
2024-06-01 16:34:28 +0300

    Add z-index to Navbar to not get overshadowed by Canvas

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,15 @@
 import { Box, Flex, HStack, Text } from '@chakra-ui/react';
+import { useTheme } from '@chakra-ui/react';
 
 import { NewEntityButtonWithPopover } from '@/features/iam_entities';
 
 interface NavbarProps {}
 
 export const Navbar: React.FC<NavbarProps> = () => {
+  const theme = useTheme();
+
   return (
-    <Box position='sticky' top='0' bg='white' p={4} boxShadow='md'>
+    <Box position='sticky' top='0' bg='white' p={4} boxShadow='md' zIndex={theme.zIndices.docked}>
       <Flex alignItems='center' justifyContent='space-between'>
         <Text fontSize='xl' fontWeight='bold' color='black'>
           IAM Project
