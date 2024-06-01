d345977064c82daebaba3697569912f7886d6269
laythra <laythra@gmail.com>
2024-06-01 16:21:56 +0300

    Cleanup home page and remove background overlay for the time being

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,41 +2,23 @@ import React from 'react';
 
 import { Flex, Box } from '@chakra-ui/react';
 
-import { Navbar } from '@/components/Navbar';
-
-import 'reactflow/dist/style.css';
 import Canvas from '@/components/Canvas';
-import { LevelsProgressionContext } from '@/components/levels_progression/LevelsProgressionProvider'; // eslint-disable-line
-import IAMEntitiesProvider from '@/components/nodes/IAMEntitiesProvider';
-import { BackgroundOverlay } from '@/components/Popover/BackgroundOverlay';
-import LeftSidePanel from '@/components/side_panels/LeftSidePanel';
-import LeftSidePanelToggleButton from '@/components/side_panels/LeftSidePanelToggleButton';
+import { Navbar } from '@/components/Navbar';
 import RightSidePanel from '@/components/side_panels/RightSidePanel';
 import RightSidePanelToggleButton from '@/components/side_panels/RightSidePanelToggleButton';
 import SidePanelProvider from '@/components/side_panels/SidePanelsProvider';
 
 const Home: React.FC = () => {
-  const isTutorialActive = LevelsProgressionContext.useSelector(
-    state => state.context.state_name === 'inside_tutorial'
-  );
-
   return (
-    <Flex direction='row' h='100vh'>
-      <Box width='80%'>
+    <Flex direction='row' h='100vh' w='100vw'>
+      <Box flex='1 0 80%' transition='flex 0.5s ease'>
         <Navbar />
-        <BackgroundOverlay isOpen={isTutorialActive} />
-        {/* <SidePanelProvider> */}
-        {/* <IAMEntitiesProvider> */}
-        {/* <LeftSidePanel /> */}
-        {/* </IAMEntitiesProvider> */}
-        {/* <LeftSidePanelToggleButton /> */}
         <Canvas />
-        {/* <RightSidePanelToggleButton /> */}
-        {/* </SidePanelProvider> */}
       </Box>
-      <Box width='20%'>
+      <SidePanelProvider>
+        <RightSidePanelToggleButton />
         <RightSidePanel />
-      </Box>
+      </SidePanelProvider>
     </Flex>
   );
 };
