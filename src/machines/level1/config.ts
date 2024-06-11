750dba1ce87df07a6b32c323a2479b602ded2cf5
laythra <laythra@gmail.com>
2024-06-11 23:53:22 +0300

    Edit level1 state machine config message

--- src/machines/level1/config.ts
+++ src/machines/level1/config.ts
@@ -1,18 +1,26 @@
 export const TUTORIAL_MESSAGES = [
   {
     popover_id: 1,
-    popover_title: 'Welcome to the tutorial!',
-    popover_content: 'This tutorial will help you through the levels',
+    popover_title: 'IAM users',
+    popover_content: `Let's begin by creating your IAM user`,
+    show_next_button: false,
   },
   {
     popover_id: 2,
-    popover_title: 'What is an IAM user',
-    popover_content: `An IAM user is a construct that allows you to interact
-      with the different amazon web services`,
+    popover_title: `Let's put your name here, shall we?`,
+    popover_content: ``,
+    show_next_button: false,
   },
   {
     popover_id: 3,
-    popover_title: 'What is an IAM group',
-    popover_content: `You can group multiple users into the same group`,
+    popover_title: `Tada! 🎉 This is the IAM user you just created!`,
+    popover_content: `Drag your IAM user around a little bit`,
+    show_next_button: true,
+  },
+  {
+    popover_id: 4,
+    popover_title: `AWS managed policy`,
+    popover_content: `This is a policy that AWS provides for you, click on the policy to view its details to the right`,
+    show_next_button: true,
   },
 ];
