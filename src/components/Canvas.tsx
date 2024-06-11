5c49dd115c23c8cef79a8216d06f99e226c45d8c
laythra <laythra@gmail.com>
2024-06-11 23:56:39 +0300

    Read nodes and edges from the state machine

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,8 +1,8 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 
 import { Box } from '@chakra-ui/react';
 import _ from 'lodash';
-import ReactFlow, { Edge, Connection, Node } from 'reactflow';
+import ReactFlow, { Edge, Connection, useNodesState, useEdgesState } from 'reactflow';
 
 import { LevelsProgressionContext } from './levels_progression/LevelsProgressionProvider';
 import DotsPattern from '@/assets/images/dots_pattern.svg';
@@ -19,48 +19,43 @@ const Canvas: React.FC = () => {
   const levelState = LevelsProgressionContext.useSelector(state => state);
   const levelActor = LevelsProgressionContext.useActorRef();
 
-  const handleDragOver = (event: React.DragEvent): void => {
-    event.preventDefault();
-  };
-
-  const addEdge = (edge: Edge): void => {
-    levelActor.send({ type: 'ADD_EDGE', edge: edge });
-  };
+  const [nodesState, setNodesState, onNodesChange] = useNodesState(levelState.context.nodes);
+  const [edgesState, setEdgesState, onEdgesChange] = useEdgesState(levelState.context.edges);
 
   const setEdges = (edges: Edge[]): void => {
     levelActor.send({ type: 'SET_EDGES', edges: edges });
   };
 
-  const addNode = (node: Node): void => {
-    levelActor.send({ type: 'ADD_IAM_NODE', node: node });
-  };
+  useEffect(() => {
+    setEdgesState(levelState.context.edges);
+  }, [levelState.context.edges]);
 
-  const handleDrop = (event: React.DragEvent): void => {
-    event.preventDefault();
+  useEffect(() => {
+    setNodesState(levelState.context.nodes);
+  }, [levelState.context.nodes]);
 
-    const nodeData = JSON.parse(event.dataTransfer.getData('json-node-data'));
-    const canvasRect = (event.target as Element).getBoundingClientRect();
+  // const handleDrop = (event: React.DragEvent): void => {
+  //   event.preventDefault();
 
-    // TODO: Handle when canvas is zoomed in/out, or simply render node in the center?
-    const canvasX = event.clientX - canvasRect.left;
-    const canvasY = event.clientY - canvasRect.top;
+  //   const nodeData = JSON.parse(event.dataTransfer.getData('json-node-data'));
+  //   const canvasRect = (event.target as Element).getBoundingClientRect();
 
-    const newNode = {
-      id: nodeData['id'],
-      type: 'iam-default',
-      position: { x: canvasX, y: canvasY },
-      data: nodeData,
-    };
+  //   // TODO: Handle when canvas is zoomed in/out, or simply render node in the center?
+  //   const canvasX = event.clientX - canvasRect.left;
+  //   const canvasY = event.clientY - canvasRect.top;
 
-    levelActor.send({ type: 'ADD_IAM_NODE', node: newNode });
-  };
+  //   const newNode = {
+  //     id: nodeData['id'],
+  //     type: 'iam-default',
+  //     position: { x: canvasX, y: canvasY },
+  //     data: nodeData,
+  //   };
+
+  //   levelActor.send({ type: 'ADD_IAM_NODE', node: newNode });
+  // };
 
   const onConnect = useCallback(
     (params: Connection) => {
-      if (levelState.context.inside_tutorial) {
-        return;
-      }
-
       const newEdge = { ...params, id: `e-${params.source}-${params.target}` } as Edge;
       let newEdges = [...levelState.context.edges, newEdge];
       setEdges(newEdges);
@@ -70,8 +65,8 @@ const Canvas: React.FC = () => {
 
         const finishedTargets = _.map(levelMetadata.connection_targets, connectionTarget => {
           if (_.differenceBy(connectionTarget.required_edges, newEdges, 'id').length === 0) {
+            // We unlock all locked edges
             _.forEach(connectionTarget.locked_edges, edge => {
-              // We unlock all locked edges
               newEdges = [...newEdges, edge];
             });
 
@@ -91,18 +86,17 @@ const Canvas: React.FC = () => {
 
   return (
     <Box
-      onDragOver={handleDragOver}
-      onDrop={handleDrop}
       backgroundColor='white'
       backgroundImage={DotsPattern}
       backgroundRepeat='repeat'
       position='relative'
       height='100vh'
-      zIndex='auto'
     >
       <ReactFlow
-        nodes={levelState.context.nodes}
-        edges={levelState.context.edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        nodes={nodesState}
+        edges={edgesState}
         onConnect={onConnect}
         nodeTypes={nodeTypes}
       />
