29b5456f9681b35028055c3d037e6b45ea48a6d7
laythra <laythra@gmail.com>
2024-06-11 23:58:06 +0300

    Send event to state machine on new entity button click

--- src/features/iam_entities/components/NewEntityButton.tsx
+++ src/features/iam_entities/components/NewEntityButton.tsx
@@ -3,6 +3,7 @@ import { IconButton, Menu, MenuButton, MenuList, MenuItem, Box } from '@chakra-u
 
 import { IdentityCreationPopup } from './IdentityCreationPopup';
 import { useIdentityCreator } from '../hooks/useIdentityCreator';
+import { LevelsProgressionContext } from '@/components/levels_progression/LevelsProgressionProvider';
 import { withPopover } from '@/decorators/withPopover';
 import { CodeEditor } from '@/features/code_editor';
 import useCodeEditor from '@/hooks/useCodeEditor';
@@ -12,13 +13,23 @@ interface NewEntityButtonProps {}
 export const NewEntityButton: React.FC<NewEntityButtonProps> = () => {
   const { openCodeEditor } = useCodeEditor();
   const { openIdentityCreator } = useIdentityCreator();
+  const levelActor = LevelsProgressionContext.useActorRef();
+
+  const hidePopovers = (): void => {
+    levelActor.send({ type: 'HIDE_POPOVERS' });
+  };
 
   return (
     <>
       <CodeEditor />
       <IdentityCreationPopup />
       <Menu>
-        <MenuButton as={IconButton} aria-label='New' icon={<PlusSquareOutlined />} />
+        <MenuButton
+          as={IconButton}
+          aria-label='New'
+          icon={<PlusSquareOutlined />}
+          onClick={hidePopovers}
+        />
         <MenuList>
           <MenuItem onClick={openIdentityCreator}>Users & Groups</MenuItem>
           <MenuItem onClick={openCodeEditor}>Roles & Policies</MenuItem>
