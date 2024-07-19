0e92929b2049345609a20e673ac84bb7f666090d
laythra <laythra@gmail.com>
2024-07-19 17:57:36 +0300

    style: Change font to lato across entire app (will potentially change)

--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,9 @@
+import '@fontsource/lato';
 import { extendTheme, ThemeConfig } from '@chakra-ui/react';
 
-export const theme: ThemeConfig = extendTheme({});
+export const theme: ThemeConfig = extendTheme({
+  fonts: {
+    heading: `Lato, sans-serif`,
+    body: `Lato, sans-serif`,
+  },
+});
