bc87b7df90dc7ef844b3a374fe2a91d35bf97a24
laythra <laythra@gmail.com>
2024-09-13 10:50:30 +0300

    feature: Add new nodes initial relative position: top-center

--- src/features/canvas/utils/nodes-position.ts
+++ src/features/canvas/utils/nodes-position.ts
@@ -3,7 +3,23 @@ import { Node, XYPosition, Viewport } from 'reactflow';
 import { theme } from '@/theme';
 import { IAMAnyNodeData } from '@/types';
 
-const VALID_INITIAL_POSITIONS = ['center'];
+const VALID_INITIAL_POSITIONS = ['center', 'top-center'];
+
+// TODO: Take into account the right side panel width and navbar height
+function getCenterCoordinates(viewport: Viewport): XYPosition {
+  const { x, y, zoom } = viewport;
+
+  const windowHeight = window.innerHeight;
+  const windowWidth = window.innerWidth;
+
+  const centerX = (windowWidth / 2 - x) / zoom;
+  const centerY = (windowHeight / 2 - y) / zoom;
+
+  return {
+    x: centerX,
+    y: centerY,
+  };
+}
 
 export function getNodeWithInitialPosition(
   node: Node<IAMAnyNodeData>,
@@ -15,35 +31,40 @@ export function getNodeWithInitialPosition(
     data: { initial_position: initialPostion },
   } = node;
 
-  const { x, y, zoom } = viewport;
-
   if (!initialPostion || !VALID_INITIAL_POSITIONS.includes(initialPostion)) {
     return node;
   }
 
-  if (initialPostion === 'center') {
-    const windowHeight = window.innerHeight;
-    const windowWidth = window.innerWidth;
-    const nodeWidth = theme.sizes.iamNodeWidthInPixels;
-    const nodeHeight = theme.sizes.iamNodeHeightInPixels;
-    const nodeSpacing = 20;
+  const nodeWidth = theme.sizes.iamNodeWidthInPixels;
+  const nodeHeight = theme.sizes.iamNodeHeightInPixels;
+  const centerCoordinates = getCenterCoordinates(viewport);
+  const nodeSpacing = 20;
 
-    const centerX =
-      (windowWidth / 2 - x) / zoom -
-      (numNodes * nodeWidth) / 2 +
-      nodeIndex * nodeWidth +
-      nodeSpacing * nodeIndex;
-    const centerY = (windowHeight / 2 - y) / zoom - nodeHeight / 2;
+  let { x, y } = centerCoordinates;
 
-    const position: XYPosition = {
-      x: centerX,
-      y: centerY,
-    };
-    return {
-      ...node,
-      position: position,
-    };
-  } else {
-    return node;
+  switch (initialPostion) {
+    case 'center':
+      x -= (numNodes * nodeWidth) / 2; // Move nodes to the left by half of the total width
+      x += nodeIndex * nodeWidth; // Move node to the right by the width of the node
+      x += nodeIndex * nodeSpacing; // Add spacing between nodes
+
+      y -= nodeHeight / 2; // Move node up by half of the height
+
+      return {
+        ...node,
+        position: { x, y },
+      };
+    case 'top-center':
+      x -= (numNodes * nodeWidth) / 2; // Move nodes to the left by half of the total width
+      x += nodeIndex * nodeWidth; // Move node to the right by the width of the node
+      x += nodeIndex * nodeSpacing; // Add spacing between nodes
+
+      y /= 2; // Move node to the top by half of the top half of the screen height
+      return {
+        ...node,
+        position: { x, y },
+      };
+    default:
+      return node;
   }
 }
