8941b5dcd370c3a75559a7363bc6b261b33c3f33
laythra <laythra@gmail.com>
2024-09-15 08:24:04 +0300

    feature: Add new left-center and right-center initial relative node positions

--- src/features/canvas/utils/nodes-position.ts
+++ src/features/canvas/utils/nodes-position.ts
@@ -3,7 +3,13 @@ import { Node, XYPosition, Viewport } from 'reactflow';
 import { theme } from '@/theme';
 import { IAMAnyNodeData } from '@/types';
 
-const VALID_INITIAL_POSITIONS = ['center', 'top-center', 'bottom-center'];
+const VALID_INITIAL_POSITIONS = [
+  'center',
+  'top-center',
+  'bottom-center',
+  'left-center',
+  'right-center',
+];
 
 // TODO: Take into account the right side panel width and navbar height
 function getCenterCoordinates(viewport: Viewport): XYPosition {
@@ -42,11 +48,18 @@ export function getNodeWithInitialPosition(
 
   let { x, y } = centerCoordinates;
 
-  if (initialPostion.includes('center')) {
+  // Stack nodes horizontally
+  if (['center', 'top-center', 'bottom-center'].includes(initialPostion)) {
     x -= (numNodes * nodeWidth) / 2; // Move nodes to the left by half of the total width
     x += nodeIndex * nodeWidth; // Move node to the right by the width of the node
     x += nodeIndex * nodeSpacing; // Add spacing between nodes
     y -= nodeHeight / 2; // Move node up by half of the height, to get its center at the center of the screen
+  } else {
+    // Stack nodes vertically
+    y -= (numNodes * nodeHeight) / 2; // Move nodes to the top by half of the total height
+    y += nodeIndex * nodeHeight; // Move node to the bottom by the height of the node
+    y += nodeIndex * nodeSpacing; // Add spacing between nodes
+    x -= nodeWidth / 2; // Move node to the left by half of the width, to get its center at the center of the screen
   }
 
   switch (initialPostion) {
@@ -67,6 +80,19 @@ export function getNodeWithInitialPosition(
         ...node,
         position: { x, y },
       };
+    case 'left-center':
+      x /= 4; // Move node to the left by half of the left half of the screen width
+      return {
+        ...node,
+        position: { x, y },
+      };
+    case 'right-center':
+      x += x / 4; // Move node to the right by half of the right half of the screen width
+      return {
+        ...node,
+        position: { x, y },
+      };
+
     default:
       return node;
   }
