711a89f6952d0bbd12332f31af6d207d936aab26
laythra <laythra@gmail.com>
2024-10-04 09:42:28 +0300

    fix: Fix establishing initial edges

--- src/machines/utils/initial-edges-resolver.ts
+++ src/machines/utils/initial-edges-resolver.ts
@@ -1,33 +1,28 @@
+import _ from 'lodash';
 import { Edge, Node } from 'reactflow';
 
 import { createEdge } from '@/factories/edge-factory';
-import { IAMUserNodeData, IAMGroupNodeData } from '@/types';
+import { IAMAnyNodeData, IAMNodeEntity, IAMPolicyNodeData, IAMUserNodeData } from '@/types';
 
-export function resolveInitialEdges(
-  initialUserNodes: Node<IAMUserNodeData>[],
-  initialGroupNodes: Node<IAMGroupNodeData>[]
-): Edge[] {
-  const policyEdges = initialUserNodes.flatMap(userNode => {
+export function resolveInitialEdges(initialNodes: Node<IAMAnyNodeData>[]): Edge[] {
+  const nodesById = _.keyBy(initialNodes, 'id');
+  const nodesByEntity = _.groupBy(initialNodes, 'data.entity');
+  const userNodes = nodesByEntity[IAMNodeEntity.User] as Node<IAMUserNodeData>[];
+
+  const policyEdges = _.flatMapDeep(userNodes, userNode => {
     return userNode.data.associated_policies.map(policyId => {
-      return createEdge({
-        source: policyId,
-        target: userNode.id,
-        sourceHandle: 'top',
-        targetHandle: 'bottom',
+      const policyToUserEdge = createEdge({ source: policyId, target: userNode.id });
+      const policyNode = nodesById[policyId] as Node<IAMPolicyNodeData>;
+
+      const userToResourceEdges = policyNode.data.resources_affected.map(resourceId => {
+        return createEdge({ source: userNode.id, target: resourceId });
       });
+
+      return [policyToUserEdge, userToResourceEdges];
     });
   });
 
-  const groupEdges = initialGroupNodes.flatMap(groupNode => {
-    return groupNode.data.attached_users.map(user => {
-      return createEdge({
-        source: user.id,
-        target: groupNode.id,
-        sourceHandle: 'top',
-        targetHandle: 'bottom',
-      });
-    });
-  });
+  // TODO: Handle edges established through group nodes
 
-  return [...policyEdges, ...groupEdges];
+  return policyEdges;
 }
