6816dc1b59d43bd95039f0ca8c996c8dd569595e
laythra <laythra@gmail.com>
2024-12-27 15:19:50 +0300

    feature: add role node factory

---
+++ src/factories/role-node-factory.ts
@@ -0,0 +1,29 @@
+import _ from 'lodash';
+import type { Node, HandleProps } from 'reactflow';
+import { Position } from 'reactflow';
+
+import { IAMNodeImage, IAMNodeEntity, IAMRoleNodeData } from '@/types';
+
+export const TEMPLATE_ROLE_NODE: Node<IAMRoleNodeData> = {
+  id: 'iam_role',
+  position: { x: 0, y: 0 },
+  type: 'iam_default',
+  draggable: true,
+  data: {
+    id: 'iam_role',
+    label: 'IAM Role',
+    entity: IAMNodeEntity.Role,
+    handles: [
+      { id: Position.Top, type: 'source', position: Position.Top },
+      { id: Position.Right, type: 'source', position: Position.Right },
+      { id: Position.Bottom, type: 'source', position: Position.Bottom },
+      { id: Position.Left, type: 'source', position: Position.Left },
+    ] as HandleProps[],
+    image: IAMNodeImage.Role,
+    initial_position: 'center',
+  } as IAMRoleNodeData,
+};
+
+export function createRoleNode(props: Partial<IAMRoleNodeData>): Node<IAMRoleNodeData> {
+  return _.merge({}, TEMPLATE_ROLE_NODE, { data: props }, { id: props.id });
+}
