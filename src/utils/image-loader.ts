833783a229fa1b6004bbf383b9cb9b1c38ef00ac
Laith Al-Rachwani <laythra@gmail.com>
2025-03-21 22:56:28 +0300

    refactor: migrate from webpack to vite (#45)

--- src/utils/image-loader.ts
+++ src/utils/image-loader.ts
@@ -1,6 +1,6 @@
 export function loadLocalImage(imagePath: string, extension: string = 'png'): string | undefined {
   try {
-    return require(`@/assets/images/${imagePath}.${extension}`);
+    return new URL(`../assets/images/${imagePath}.${extension}`, import.meta.url).href;
   } catch (e) {
     // TODO: Load placeholder image
     return undefined;
