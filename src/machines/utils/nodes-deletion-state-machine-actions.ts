bf098b3bd5f7a89587c6c34dd5865c6bb525f8e7
Laith Al-Rachwani <laythra@gmail.com>
2025-04-04 15:44:25 +0300

    refactor: move creating a role node into nodes-creation-state-machine-actions (#74)

--- src/machines/utils/nodes-deletion-state-machine-actions.ts
+++ src/machines/utils/nodes-deletion-state-machine-actions.ts
@@ -1,7 +1,9 @@
 import { produce } from 'immer';
 
+import { deleteConnectionEdges } from './edges-deletion-state-machine-actions';
 import { BaseFinishEventMap, GenericContext } from '../types';
 import { IAMAnyNode } from '@/types/iam-node-types';
+import { getEdgeName } from '@/utils/names';
 
 /**
  * Deletes an edge from the context and all its children
@@ -15,9 +17,19 @@ export function deleteNode<TLevelObjectiveID, TFinishEventMap extends BaseFinish
   context: GenericContext<TLevelObjectiveID, TFinishEventMap>,
   nodeToDelete: IAMAnyNode
 ): { updatedContext: GenericContext<TLevelObjectiveID, TFinishEventMap> } {
-  const updatedContext = produce(context, draftContext => {
+  let updatedContext = produce(context, draftContext => {
     draftContext.nodes = draftContext.nodes.filter(node => node.id !== nodeToDelete.id);
   });
+  const edgesToDelete = updatedContext.nodes_connnections
+    .filter(
+      connection => connection.from.id === nodeToDelete.id || connection.to.id === nodeToDelete.id
+    )
+    .map(connection => getEdgeName(connection.from.id, connection.to.id));
+
+  ({ updatedContext } = deleteConnectionEdges<TLevelObjectiveID, TFinishEventMap>(
+    updatedContext,
+    edgesToDelete
+  ));
 
   return { updatedContext };
 }
