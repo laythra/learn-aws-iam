91a6c3b26080bb2f120ae5c549426ae024fa62df
laythra <laythra@gmail.com>
2025-04-07 23:12:49 +0300

    tests: add create node factory tests

---
+++ src/factories/nodes/create-node-factory.test.ts
@@ -0,0 +1,141 @@
+import { HandleProps, Position } from '@xyflow/react';
+import { describe, it, expect } from 'vitest';
+
+import { createNodeFactory } from './create-node-factory';
+import { getNodeAnimations, NODE_ANIMATION_ID } from '@/config/node-animations';
+import { IAMAnyNode, IAMNodeEntity } from '@/types';
+
+describe('createNodeFactory', () => {
+  const defaultHandles: HandleProps[] = [
+    { id: 'handle-1', position: Position.Top, type: 'source' },
+    { id: 'handle-2', position: Position.Bottom, type: 'source' },
+  ];
+
+  const baseConfig = {
+    type: 'custom-node',
+    entity: IAMNodeEntity.User,
+    image: 'custom-image.png',
+    defaultHandles,
+    width: 300,
+    height: 100,
+    deletable: true,
+    draggable: false,
+    initial_position: 'top-left',
+    additionalData: { customField: 'customValue' },
+  };
+
+  const baseExpectedData = {
+    entity: IAMNodeEntity.User,
+    image: 'custom-image.png',
+    initial_position: 'top-left',
+  };
+
+  const factory = createNodeFactory(baseConfig);
+
+  it('should create a node with default configuration', () => {
+    const node = factory({ dataOverrides: { id: 'default-id' } });
+
+    expect(node).toEqual({
+      id: 'default-id',
+      position: { x: 0, y: 0 },
+      type: 'custom-node',
+      draggable: false,
+      deletable: true,
+      width: 300,
+      height: 100,
+      data: {
+        id: 'default-id',
+        label: IAMNodeEntity.User,
+        handles: defaultHandles,
+        customField: 'customValue',
+        animations: getNodeAnimations(NODE_ANIMATION_ID.ShimmerBackground),
+        ...baseExpectedData,
+      },
+    });
+  });
+
+  it('should override root properties', () => {
+    const rootOverrides = { draggable: true, type: 'resource' } satisfies Partial<IAMAnyNode>;
+    const node = factory({
+      rootOverrides,
+      dataOverrides: { id: 'custom-id' },
+    });
+
+    expect(node).toEqual({
+      id: 'custom-id',
+      position: { x: 0, y: 0 },
+      deletable: true,
+      width: 300,
+      height: 100,
+      ...rootOverrides,
+      data: {
+        id: 'custom-id',
+        label: IAMNodeEntity.User,
+        handles: defaultHandles,
+        animations: getNodeAnimations(NODE_ANIMATION_ID.ShimmerBackground),
+        customField: 'customValue',
+        ...baseExpectedData,
+      },
+    });
+  });
+
+  it('should override data properties', () => {
+    const dataOverrides = {
+      id: 'default-id',
+      customField: 'custom-value',
+      label: "musashi, you've become kinder",
+      handles: [],
+      animations: {},
+    } satisfies Partial<IAMAnyNode['data']>;
+    const node = factory({
+      dataOverrides,
+    });
+
+    expect(node).toEqual({
+      id: 'default-id',
+      position: { x: 0, y: 0 },
+      type: 'custom-node',
+      draggable: false,
+      deletable: true,
+      width: 300,
+      height: 100,
+      data: {
+        ...baseExpectedData,
+        ...dataOverrides,
+      },
+    });
+  });
+
+  it('should combine root and data overrides', () => {
+    const rootOverrides = {
+      width: 400,
+      draggable: true,
+    } satisfies Partial<IAMAnyNode>;
+
+    const dataOverrides = {
+      id: 'data-override-id',
+      customField: 'combinedValue',
+      label: 'Overridden Label',
+      handles: [],
+      animations: {},
+    } satisfies Partial<IAMAnyNode['data']>;
+
+    const node = factory({
+      rootOverrides,
+      dataOverrides,
+    });
+
+    expect(node).toEqual({
+      id: 'data-override-id',
+      position: { x: 0, y: 0 },
+      type: 'custom-node',
+      deletable: true,
+      height: 100,
+      ...rootOverrides,
+      data: {
+        ...baseExpectedData,
+        ...dataOverrides,
+      },
+    });
+  });
+});
