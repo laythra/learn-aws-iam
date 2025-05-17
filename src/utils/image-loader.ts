4df43b47418491bc69122dc7e2aad2ebc9c1a244
laythra <laythra@gmail.com>
2025-05-17 10:56:31 +0300

    build: load local images at build time with vite

--- src/utils/image-loader.ts
+++ src/utils/image-loader.ts
@@ -1,8 +1,6 @@
+const images = import.meta.glob('../assets/images/**/*.{png,jpg,gif}', { eager: true });
+
 export function loadLocalImage(imagePath: string, extension: string = 'png'): string | undefined {
-  try {
-    return new URL(`../assets/images/${imagePath}.${extension}`, import.meta.url).href;
-  } catch (e) {
-    // TODO: Load placeholder image
-    return undefined;
-  }
+  const key = `../assets/images/${imagePath}.${extension}`;
+  return (images as Record<string, { default: string }>)[key]?.default;
 }
