0607673cfef8d308807346cc498f2c20dedc81db
laythra <laythra@gmail.com>
2025-06-30 23:12:52 +0400

    chore(playwright): increase workers count to 2 for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,8 +18,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   retries: 0,
-  /* Opt out of parallel tests on CI. */
-  workers: undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
