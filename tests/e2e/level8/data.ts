f586e2c6edc59295ede14ddbe210954d28478027
laythra <laythra@gmail.com>
2025-07-07 09:08:33 +0300

    test: move e2e test solutions into separate files per policy

--- tests/e2e/level8/data.ts
+++ tests/e2e/level8/data.ts
@@ -10,9 +10,13 @@
  * Please go through the tutorial step by step instead! 🎓
  */
 
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export const ENCODED_TEST_SOLUTIONS = {
-  // eslint-disable-next-line
-  '1': 'H4sIAAAAAAAAE32PzWrDMBCE32WPRQLL/Umrm3FLLznZkEvIQcgbIyJLRis3FON3L5ILdQvpcXdmdr6d4YCBjHcgoSxEyUXBxQ4YtFFFHNBFkMcZ3s5n1BEkVNb6KzCodMyhI2jfYYej9Z+ywd5QxFCNozVaJUeDHyafZ1tjHVBFfM1D7vilvmO8Je0NbTSCE4MGyU9BI0hQwUl1JbkJTMRRUeRCivL+4fFp9/xSiFKqH0LZWqUvte++eapxhIX9+zKhDhhpUE71GBJvmzcHZSe8wfQnc4NrdUlKSNy4iH3IkHwVgEHtXWdWkBnaGIzr9+aCaUo1E2FwakjNhM74wO9gWZbT8gUx3L/t6QEAAA==',
-  // eslint-disable-next-line
-  '2': 'H4sIAAAAAAAAE32PzU7DMBCE32XPjojDT8G3KFRcOCWol6qHlbONLFw7eB0qFOXdkRMQAakcd2dm59sRdhTYeAcKilwWmcwzuQEBTcRIJ3IR1H6E7fFIOoKC0lp/BgGljnNoD9q31FJv/YeqqTMcKZR9b43G5Kjp3cznxdpYBcJIj/Mwd/xSnyhekp4NrzSGg4Ca2A9BEyjA4BSeWa0CA2eEHDOpZHF9c3u3uX/IZaHwh1A1FvVr5dsvnrLvYRL/vsykA0U+ocOOQuJt5s0O7UAXmP5kLnAtLsUJKTMuUhdmyGwRQEDlXWsWkBGaGIzrtm8DWk5zKvrufsHuKnibGJic8QGmaTpMn0OaIY7xAQAA',
+  policy1: () => readFile(join(__dirname, 'policies', 'policy1.txt'), { encoding: 'utf-8' }),
+  policy2: () => readFile(join(__dirname, 'policies', 'policy2.txt'), { encoding: 'utf-8' }),
 };
