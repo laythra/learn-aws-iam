f83bf386a4216b2192bdcc9e958a24071afa1763
laythra <laythra@gmail.com>
2025-07-18 20:00:46 +0300

    ci: disable playwright's WebKit and Firefox in CI to speed up GitHub Actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,16 +42,19 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // Disabling these browsers for now, as they increase CI time significantly.
+    ...(!process.env.CI
+      ? [
+          {
+            name: 'firefox',
+            use: { ...devices['Desktop Firefox'] },
+          },
+          {
+            name: 'webkit',
+            use: { ...devices['Desktop Safari'] },
+          },
+        ]
+      : []),
 
     /* Test against mobile viewports. */
     // {
