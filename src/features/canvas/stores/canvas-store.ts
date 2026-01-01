ce83d351e2ad873a359eed307191a73a63e1c63f
laith <laythra@gmail.com>
2026-01-01 23:40:03 +0300

    chore: upgrade xstate and xstate/store to the latest stable

--- src/features/canvas/stores/canvas-store.ts
+++ src/features/canvas/stores/canvas-store.ts
@@ -1,4 +1,4 @@
-import { createStoreWithProducer } from '@xstate/store';
+import { createStore } from '@xstate/store';
 import {
   type NodeChange,
   type EdgeChange,
@@ -64,14 +64,14 @@ type CanvasStoreEvents = {
  * CanvasStore manages the state of nodes and edges on the IAM canvas.
  * It handles actions such as adding, updating, deleting nodes/edges,
  * positioning nodes, and managing selection and hover states.
- * The source of truth for nodes and edges is remains inside the state machine within each level
+ * The source of truth for nodes and edges remains inside the state machine within each level
  * and this store acts as a synchronized view for the React Flow canvas. Emitting events too frequently to the
  * fully fledged state machine can lead to performance issues, such as when the user is dragging nodes around
- * or hovering over edges. Therefore, this store is optimized for responsiveness while keeping in sync with the state machine.
+ * or hovering over edges. Therefore, a lightweight xstate store is more suitable for responsiveness while keeping in sync with the state machine.
  * Uses Immer for convenient immutable state updates.
  * @returns {CanvasStore} The CanvasStore instance for managing canvas state.
  */
-export const CanvasStore = createStoreWithProducer<CanvasStoreState, CanvasStoreEvents>(produce, {
+export const CanvasStore = createStore<CanvasStoreState, CanvasStoreEvents, never>({
   context: {
     nodes: [],
     edges: [],
@@ -79,63 +79,70 @@ export const CanvasStore = createStoreWithProducer<CanvasStoreState, CanvasStore
     edgeIdsWithDeletionInProgress: new Set<string>(),
   },
   on: {
-    changeNodesState: (ctx: CanvasStoreState, event: { changes: NodeChange<IAMAnyNode>[] }) => {
-      ctx.nodes = applyNodeChanges(event.changes, ctx.nodes);
-    },
-    changeEdgesState: (ctx: CanvasStoreState, event: { changes: EdgeChange<IAMEdge>[] }) => {
-      ctx.edges = applyEdgeChanges(event.changes, ctx.edges);
-    },
-    hoverOverEdge: (ctx: CanvasStoreState, event: { edgeId?: string }) => {
+    changeNodesState: produce(
+      (ctx: CanvasStoreState, event: { changes: NodeChange<IAMAnyNode>[] }) => {
+        ctx.nodes = applyNodeChanges(event.changes, ctx.nodes);
+      }
+    ),
+    changeEdgesState: produce(
+      (ctx: CanvasStoreState, event: { changes: EdgeChange<IAMEdge>[] }) => {
+        ctx.edges = applyEdgeChanges(event.changes, ctx.edges);
+      }
+    ),
+    hoverOverEdge: produce((ctx: CanvasStoreState, event: { edgeId?: string }) => {
       ctx.hoveredOverEdgeId = event.edgeId;
-    },
-    selectEdge: (ctx: CanvasStoreState, event: { edgeId?: string }) => {
+    }),
+    selectEdge: produce((ctx: CanvasStoreState, event: { edgeId?: string }) => {
       ctx.selectedEdgeId = event.edgeId;
-    },
-    setNodes: (ctx: CanvasStoreState, event: { nodes: IAMAnyNode[] } & NodesPositioningParams) => {
-      // Position and set nodes
-      ctx.nodes = positionNewNodes(
-        [],
-        event.nodes,
-        event.layoutGroups,
-        event.sidePanelWidth,
-        event.reactFlowViewport
-      );
-    },
-    setEdges: (ctx: CanvasStoreState, event: { edges: IAMEdge[] }) => {
+    }),
+    setNodes: produce(
+      (ctx: CanvasStoreState, event: { nodes: IAMAnyNode[] } & NodesPositioningParams) => {
+        // Position and set nodes
+        ctx.nodes = positionNewNodes(
+          [],
+          event.nodes,
+          event.layoutGroups,
+          event.sidePanelWidth,
+          event.reactFlowViewport
+        );
+      }
+    ),
+    setEdges: produce((ctx: CanvasStoreState, event: { edges: IAMEdge[] }) => {
       ctx.edges = event.edges;
-    },
-    updateNodePosition(
-      ctx: CanvasStoreState,
-      event: { nodeId: string; position: { x: number; y: number } }
-    ) {
-      ctx.nodes.find(nd => nd.id === event.nodeId)!.position = event.position;
-    },
-    updateSelectedNodeId(ctx: CanvasStoreState, event: { nodeId: string }) {
+    }),
+    updateNodePosition: produce(
+      (ctx: CanvasStoreState, event: { nodeId: string; position: { x: number; y: number } }) => {
+        ctx.nodes.find(nd => nd.id === event.nodeId)!.position = event.position;
+      }
+    ),
+    updateSelectedNodeId: produce((ctx: CanvasStoreState, event: { nodeId: string }) => {
       ctx.selectedNodeId = event.nodeId;
       ctx.nodes.forEach(node => (node.zIndex = 0));
       ctx.nodes.find(node => node.id == event.nodeId)!.zIndex = 10000;
-    },
-    openNodePanel(
-      ctx: CanvasStoreState,
-      event: { nodeId: string; panel: 'content' | 'tags' | 'arn' | 'users-list' | undefined }
-    ) {
-      ctx.selectedNodeId = event.nodeId;
-      ctx.nodeIdWithOpenedContent = event.panel === 'content' ? event.nodeId : undefined;
-      ctx.nodeIdWithOpenedTags = event.panel === 'tags' ? event.nodeId : undefined;
-      ctx.nodeIdWithOpenedARN = event.panel === 'arn' ? event.nodeId : undefined;
-      ctx.nodeIdWithOpenedUsersList = event.panel === 'users-list' ? event.nodeId : undefined;
-    },
-    closeAllNodePanels(ctx: CanvasStoreState) {
+    }),
+    openNodePanel: produce(
+      (
+        ctx: CanvasStoreState,
+        event: { nodeId: string; panel: 'content' | 'tags' | 'arn' | 'users-list' | undefined }
+      ) => {
+        ctx.selectedNodeId = event.nodeId;
+        ctx.nodeIdWithOpenedContent = event.panel === 'content' ? event.nodeId : undefined;
+        ctx.nodeIdWithOpenedTags = event.panel === 'tags' ? event.nodeId : undefined;
+        ctx.nodeIdWithOpenedARN = event.panel === 'arn' ? event.nodeId : undefined;
+        ctx.nodeIdWithOpenedUsersList = event.panel === 'users-list' ? event.nodeId : undefined;
+      }
+    ),
+    closeAllNodePanels: produce((ctx: CanvasStoreState) => {
       ctx.nodeIdWithOpenedContent = undefined;
       ctx.nodeIdWithOpenedTags = undefined;
       ctx.nodeIdWithOpenedARN = undefined;
       ctx.nodeIdWithOpenedUsersList = undefined;
-    },
-    clearCanvas(ctx: CanvasStoreState) {
+    }),
+    clearCanvas: produce((ctx: CanvasStoreState) => {
       ctx.nodes = [];
       ctx.edges = [];
-    },
-    toggleAccountCollapse(ctx: CanvasStoreState, event: { accountId: string }) {
+    }),
+    toggleAccountCollapse: produce((ctx: CanvasStoreState, event: { accountId: string }) => {
       const accountNode = ctx.nodes.find(
         node => node.id === event.accountId && node.data.entity === IAMNodeEntity.Account
       );
@@ -149,8 +156,8 @@ export const CanvasStore = createStoreWithProducer<CanvasStoreState, CanvasStore
       childNodes.forEach(childNode => {
         childNode.hidden = !isCollapsed;
       });
-    },
-    markEdgesForDeletion(ctx: CanvasStoreState, event: { edgeIds: string[] }) {
+    }),
+    markEdgesForDeletion: produce((ctx: CanvasStoreState, event: { edgeIds: string[] }) => {
       // Making edges non-interactive to avoid race conditions during deletion
       // I understand there might be other race conditions, but I won't delve into them now
       ctx.edges.forEach(edge => {
@@ -166,8 +173,8 @@ export const CanvasStore = createStoreWithProducer<CanvasStoreState, CanvasStore
       event.edgeIds.forEach(id => {
         ctx.edgeIdsWithDeletionInProgress.add(id);
       });
-    },
-    markNodesForDeletion(ctx: CanvasStoreState, event: { nodeIds: string[] }) {
+    }),
+    markNodesForDeletion: produce((ctx: CanvasStoreState, event: { nodeIds: string[] }) => {
       // Making edges non-interactive to avoid race conditions during deletion
       ctx.nodes.forEach(node => {
         if (event.nodeIds.includes(node.id)) {
@@ -179,57 +186,61 @@ export const CanvasStore = createStoreWithProducer<CanvasStoreState, CanvasStore
       event.nodeIds.forEach(id => {
         ctx.nodeIdsWithDeletionInProgress.add(id);
       });
-    },
-    finalizeEdgesDeletion(ctx: CanvasStoreState, event: { edgeIds: string[] }) {
+    }),
+    finalizeEdgesDeletion: produce((ctx: CanvasStoreState, event: { edgeIds: string[] }) => {
       ctx.edges = ctx.edges.filter(edge => !event.edgeIds.includes(edge.id));
       event.edgeIds.forEach(id => ctx.edgeIdsWithDeletionInProgress.delete(id));
-    },
-    finalizeNodesDeletion(ctx: CanvasStoreState, event: { nodeIds: string[] }) {
+    }),
+    finalizeNodesDeletion: produce((ctx: CanvasStoreState, event: { nodeIds: string[] }) => {
       ctx.nodes = ctx.nodes.filter(node => !event.nodeIds.includes(node.id));
       event.nodeIds.forEach(id => ctx.nodeIdsWithDeletionInProgress.delete(id));
-    },
-    addEdges(ctx: CanvasStoreState, event: { edges: IAMEdge[] }) {
+    }),
+    addEdges: produce((ctx: CanvasStoreState, event: { edges: IAMEdge[] }) => {
       ctx.edges.push(...event.edges);
-    },
-    addNodes(ctx: CanvasStoreState, event: { nodes: IAMAnyNode[] } & NodesPositioningParams) {
-      const positionedNodes = positionNewNodes(
-        ctx.nodes,
-        event.nodes,
-        event.layoutGroups,
-        event.sidePanelWidth,
-        event.reactFlowViewport
-      );
+    }),
+    addNodes: produce(
+      (ctx: CanvasStoreState, event: { nodes: IAMAnyNode[] } & NodesPositioningParams) => {
+        const positionedNodes = positionNewNodes(
+          ctx.nodes,
+          event.nodes,
+          event.layoutGroups,
+          event.sidePanelWidth,
+          event.reactFlowViewport
+        );
 
-      ctx.nodes.push(...positionedNodes);
-    },
-    updateNodeData(ctx: CanvasStoreState, event: { node: IAMAnyNode }) {
-      ctx.nodes.find(n => n.id === event.node.id)!.data = event.node.data;
-    },
-    adjustForSidePanelWidthChange(
-      ctx: CanvasStoreState,
-      event: {
-        sidePanelWidth: number;
-        viewport: Viewport;
-        nodeWidth: number;
+        ctx.nodes.push(...positionedNodes);
       }
-    ) {
-      // Calculate the side panel position in flow coordinates
-      const sidePanelPosX =
-        (window.innerWidth - event.sidePanelWidth - event.viewport.x) / event.viewport.zoom;
-
-      ctx.nodes = ctx.nodes.map(node => {
-        if (node.position.x + event.nodeWidth >= sidePanelPosX) {
-          return {
-            ...node,
-            position: {
-              ...node.position,
-              x: sidePanelPosX - 10 - event.nodeWidth,
-            },
-          };
+    ),
+    updateNodeData: produce((ctx: CanvasStoreState, event: { node: IAMAnyNode }) => {
+      ctx.nodes.find(n => n.id === event.node.id)!.data = event.node.data;
+    }),
+    adjustForSidePanelWidthChange: produce(
+      (
+        ctx: CanvasStoreState,
+        event: {
+          sidePanelWidth: number;
+          viewport: Viewport;
+          nodeWidth: number;
         }
+      ) => {
+        // Calculate the side panel position in flow coordinates
+        const sidePanelPosX =
+          (window.innerWidth - event.sidePanelWidth - event.viewport.x) / event.viewport.zoom;
 
-        return node;
-      });
-    },
+        ctx.nodes = ctx.nodes.map(node => {
+          if (node.position.x + event.nodeWidth >= sidePanelPosX) {
+            return {
+              ...node,
+              position: {
+                ...node.position,
+                x: sidePanelPosX - 10 - event.nodeWidth,
+              },
+            };
+          }
+
+          return node;
+        });
+      }
+    ),
   },
 });
