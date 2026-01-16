208484ca4b98ea11e7c0bf4a5948b1a2a1e95365
laith <laythra@gmail.com>
2026-01-16 23:46:53 +0300

    feat(level4): enhance level 4 tutorial flow

--- src/machines/level4/tutorial_messages/popover-tutorial-messages.ts
+++ src/machines/level4/tutorial_messages/popover-tutorial-messages.ts
@@ -1,33 +1,19 @@
-import { PolicyNodeID } from '../types/node-id-enums';
 import { ElementID } from '@/config/element-ids';
 import type { PopoverTutorialMessage } from '@/machines/types/tutorial-message-types';
 
 const POPOVER_MSG1 = `
-  This Level requires Editing existing **Customer Managed Policies**
-  to ensure that the permissions are correct.
-
-  You should be able to see an Edit button on the right side of the policy's content.
-`;
-
-const POPOVER_MSG2 = `
   As usual, you'll find the list of objectives in the right side panel. Good Luck! 💪 |lg
+
+  ***Click 'Next' to begin your mission!***
 `;
 
 export const POPOVER_TUTORIAL_MESSAGES: PopoverTutorialMessage[] = [
   {
-    element_id: PolicyNodeID.DeveloperPolicy,
-    popover_title: 'Editing Policies',
-    popover_content: POPOVER_MSG1,
-    show_next_button: false,
-    show_close_button: false,
-    popover_placement: 'top',
-  },
-  {
     element_id: ElementID.RightSidePanelToggleButton,
     popover_title: "You're on your own",
-    popover_content: POPOVER_MSG2,
-    show_next_button: false,
-    show_close_button: true,
+    popover_content: POPOVER_MSG1,
+    show_next_button: true,
+    show_close_button: false,
     popover_placement: 'top',
   },
 ];
