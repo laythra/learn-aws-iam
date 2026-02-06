9dbd9cb791a42c1e36e13d27b435c5bdfdc20c16
laith <laythra@gmail.com>
2026-02-06 21:32:08 +0300

    feat: add level picker button to navbar

--- src/app_shell/navbar/NavbarControls.tsx
+++ src/app_shell/navbar/NavbarControls.tsx
@@ -1,6 +1,7 @@
 import { HStack } from '@chakra-ui/react';
 
 import { GoToCheckpointButton } from './GoToCheckpointButton';
+import { LevelPickerButton } from './LevelPickerButton';
 import { NewEntityButton } from './NewEntityButton';
 import { RestartLevelButton } from './RestartLevelButton';
 
@@ -11,7 +12,7 @@ export const NavbarControls: React.FC<NavbarControlsProps> = () => {
     <HStack spacing={4}>
       <GoToCheckpointButton />
       <RestartLevelButton />
-      {/* <LevelPickerButton /> */}
+      <LevelPickerButton />
       <NewEntityButton />
     </HStack>
   );
