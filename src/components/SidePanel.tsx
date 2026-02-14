546ce60a4b5534c10710ba3bf7bebe621f4a26d3
laith <laythra@gmail.com>
2026-02-14 23:07:42 +0300

    Revert "refactor: simplify SidePanel nesting"
    This reverts commit 7e5238ba5c35fb54f1bb4ad34577fd7ca2d87735.

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -1,4 +1,4 @@
-import { Box, useTheme } from '@chakra-ui/react';
+import { Flex, Box, useTheme } from '@chakra-ui/react';
 
 interface SidePanelProps {
   children: React.ReactNode;
@@ -12,22 +12,28 @@ const SidePanel: React.FC<SidePanelProps> = ({ children, isOpen, transitionDurat
   const width = isOpen ? openWidth : '0%';
 
   return (
-    <Box
+    <Flex
       h={`calc(100vh - ${theme.sizes.navbarHeightInPixels})`}
       mt={theme.sizes.navbarHeightInPixels}
+      direction='row'
+      flexGrow={0}
+      flexShrink={0}
       w={width}
-      transition={`
-        width ${transitionDuration}ms ease-in-out,
-        opacity ${transitionDuration}ms ease-in-out`}
+      transition={`width ${transitionDuration}ms ease-in-out`}
       overflow='hidden'
-      overflowY='auto'
-      px={4}
-      py={4}
-      opacity={isOpen ? 1 : 0}
-      pointerEvents={isOpen ? 'auto' : 'none'}
     >
-      {children}
-    </Box>
+      <Box
+        flex='1'
+        overflowY='auto'
+        px={4}
+        py={4}
+        opacity={isOpen ? 1 : 0}
+        transition={`opacity ${transitionDuration}ms ease-in-out`}
+        pointerEvents={isOpen ? 'auto' : 'none'}
+      >
+        {children}
+      </Box>
+    </Flex>
   );
 };
 
