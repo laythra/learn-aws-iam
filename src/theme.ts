e0a83767b18447b5cfde7d9d8ac7502d16ff3f98
laythra <laythra@gmail.com>
2026-02-28 20:08:38 +0300

    fix(ui): fix z-index issues for node buttons' popovers and toast

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,15 @@ import { extendTheme } from '@chakra-ui/react';
 import { CustomTheme } from './types/custom-theme';
 
 export const theme: CustomTheme = extendTheme({
+  styles: {
+    global: {
+      ':root': {
+        // Used to override the default z-index of chakra's toast component
+        // It should be less than the z-index of modals to show underneath them
+        '--toast-z-index': '900',
+      },
+    },
+  },
   fonts: {
     heading: `Lato, sans-serif`,
     body: `Lato, sans-serif`,
@@ -15,5 +24,17 @@ export const theme: CustomTheme = extendTheme({
     navbarHeightInPixels: 65,
     codeEditorHeightInPixels: 500,
     sidePanelWidthInPixels: window.innerWidth * 0.2,
+  },
+  components: {
+    Popover: {
+      baseStyle: {
+        popper: { zIndex: 1000 },
+      },
+      variants: {
+        elevated: {
+          popper: { zIndex: 1100 },
+        },
+      },
+    },
   },
 }) as CustomTheme;
